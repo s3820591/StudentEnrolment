285531563251cefdac6d51a25a6c3861335578bc
ASUS <s3820591@rmit.edu.vn>
2022-04-06 18:57:29 +0700

    Last of last of last

--- src/StudentEnrolmentManager.java
+++ src/StudentEnrolmentManager.java
@@ -1,9 +1,9 @@
 public interface StudentEnrolmentManager {
-    public void add();
-    public void update();
-    public void delete();
-    public void getOne();
-    public void getAll();
+    void add();
+    void update();
+    void delete();
+    void getOne();
+    void getAll();
 
 
 }
